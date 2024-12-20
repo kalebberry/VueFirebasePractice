50b0e37980ee05e74c9fcf6d58036fd152bda69b
Kaleb Berry <kaleb.berry@outlook.com>
2024-12-19 20:28:33 -0600

    init commit?

---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+	props: ['theme'],
+};
+</script>
+
+<template>
+	<div class="backdrop">
+		<div class="modal" :class="{ sale: theme === 'sale' }">
+			<slot></slot>
+			<div class="actions">
+				<slot name="links"></slot>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.modal {
+	width: 400px;
+	padding: 20px;
+	margin: 100px auto;
+	background: white;
+	border-radius: 10px;
+}
+
+.backdrop {
+	top: 0;
+	position: fixed;
+	background: rgba(0, 0, 0, 0.5);
+	width: 100%;
+	height: 100%;
+}
+
+.sale {
+	color: red;
+}
+</style>
